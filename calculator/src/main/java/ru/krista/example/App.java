9c8f71f48fd3fe0a8ba12c7dfc4426fa3ba69a60
1kisra <arseniydomshchikov@gmail.com>
2024-03-07 09:28:05 +0300

    hel

--- calculator/src/main/java/ru/krista/example/App.java
+++ calculator/src/main/java/ru/krista/example/App.java
@@ -2,18 +2,10 @@ package ru.krista.example;
 
 import java.util.Scanner;
 
-/**
- * Hello world!
- */
 public final class App {
 
     private App() {
     }
-
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
 
     public static void main(String[] args) {
         System.out.println("Калькулятор");
